3ebcaa1c961ad249c2ff7f499cfbfa651596380d
AleLeonMarin <aleleonmarin01@gmail.com>
2024-04-27 20:32:02 -0600

    [UPDATED]

---
+++ src/main/java/cr/ac/una/proyecto/controller/PreguntaController.java
@@ -0,0 +1,34 @@
+package cr.ac.una.proyecto.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.CornerRadii;
+
+/**
+ * FXML Controller class
+ *
+ * @author justi
+ */
+public class PreguntaController extends Controller implements Initializable {
+
+    @FXML
+    private Button joya;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @Override
+    public void initialize() {
+        // TODO Auto-generated method stub
+        
+    }    
+    
+}
