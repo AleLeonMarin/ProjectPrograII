4bb0720afc17a63547bff7aebe3404e1d2759362
AleLeonMarin <aleleonmarin01@gmail.com>
2024-04-27 20:21:40 -0600

    [INIT] Project basis

---
+++ proyecto/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module cr.ac.una.proyecto {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.logging;
+    requires MaterialFX;
+
+    opens cr.ac.una.proyecto to javafx.fxml;
+    exports cr.ac.una.proyecto;
+    exports cr.ac.una.proyecto.controller;
+    opens cr.ac.una.proyecto.controller to javafx.fxml;
+    //exports cr.ac.una.proyecto.model;
+    //exports cr.ac.una.proyecto.service;
+    exports cr.ac.una.proyecto.util;
+}
