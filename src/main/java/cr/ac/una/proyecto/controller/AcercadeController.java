7c365c21696cdad614bc4f3014bd32e202579cd0
JustinMdz <justin.mendez.mena@est.una.ac.cr>
2024-05-01 23:32:38 -0600

    add DB Model In Project

---
+++ src/main/java/cr/ac/una/proyecto/controller/AcercadeController.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package cr.ac.una.proyecto.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class AcercadeController implements Initializable {
+
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+}
