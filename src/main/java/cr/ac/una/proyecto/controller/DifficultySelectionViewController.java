cfe7125b9941e5a015252b0de0f16bb88bb66ccb
AleLeonMarin <aleleonmarin01@gmail.com>
2024-05-03 11:04:36 -0600

    [UI] UI IMP

---
+++ src/main/java/cr/ac/una/proyecto/controller/DifficultySelectionViewController.java
@@ -0,0 +1,81 @@
+package cr.ac.una.proyecto.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import cr.ac.una.proyecto.util.FlowController;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXCheckbox;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ToggleGroup;
+
+public class DifficultySelectionViewController extends Controller implements Initializable {
+
+    @FXML
+    private MFXButton btnPlay;
+
+    @FXML
+    private MFXCheckbox ckbDuel;
+
+    @FXML
+    private MFXCheckbox ckbEasy;
+
+    @FXML
+    private MFXCheckbox ckbHard;
+
+    @FXML
+    private MFXCheckbox ckbMedium;
+
+    @FXML
+    void onActionBtnPlay(ActionEvent event) {
+        FlowController.getInstance().goViewInWindow("TableroView");
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void initialize() {
+
+        ckbEasy.selectedProperty().addListener((obs, wasSelected, isNowSelected) -> {
+            if (isNowSelected) {
+                ckbMedium.setSelected(false);
+                ckbHard.setSelected(false);
+                ckbDuel.setSelected(false);
+            }
+        });
+
+        ckbMedium.selectedProperty().addListener((obs, wasSelected, isNowSelected) -> {
+            if (isNowSelected) {
+                ckbEasy.setSelected(false);
+                ckbHard.setSelected(false);
+                ckbDuel.setSelected(false);
+            }
+        });
+
+        ckbHard.selectedProperty().addListener((obs, wasSelected, isNowSelected) -> {
+            if (isNowSelected) {
+                ckbEasy.setSelected(false);
+                ckbMedium.setSelected(false);
+                ckbDuel.setSelected(false);
+            }
+        });
+
+        ckbDuel.selectedProperty().addListener((obs, wasSelected, isNowSelected) -> {
+            if (isNowSelected) {
+                ckbEasy.setSelected(false);
+                ckbMedium.setSelected(false);
+                ckbHard.setSelected(false);
+            }
+        });
+    }
+
+
+
+}
--- src/main/java/cr/ac/una/proyecto/controller/RegistryNewGameViewController.java
+++ src/main/java/cr/ac/una/proyecto/controller/RegistryNewGameViewController.java
@@ -3,14 +3,16 @@ package cr.ac.una.proyecto.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import cr.ac.una.proyecto.util.FlowController;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXSlider;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.stage.Stage;
 
-public class RegistryNewGameViewController extends Controller implements Initializable{
+public class RegistryNewGameViewController extends Controller implements Initializable {
 
     @FXML
     private MFXButton btnNext;
@@ -39,6 +41,9 @@ public class RegistryNewGameViewController extends Controller implements Initial
     @FXML
     void onActionBtnNext(ActionEvent event) {
 
+        FlowController.getInstance().goViewInWindow("DifficultySelectionView");
+        ((Stage) btnNext.getScene().getWindow()).close();
+
     }
 
     @Override
@@ -48,7 +53,34 @@ public class RegistryNewGameViewController extends Controller implements Initial
 
     @Override
     public void initialize() {
-        // TODO Auto-generated method stub
+
+        sldQty.setMin(2);
+        sldQty.setMax(6);
+        sldQty.setValue(2);
+        txfJug3.setDisable(true);
+        txfJug4.setDisable(true);
+        txfJug5.setDisable(true);
+        txfJug6.setDisable(true);
+        txfJug3.setVisible(false);
+        txfJug4.setVisible(false);
+        txfJug5.setVisible(false);
+        txfJug6.setVisible(false);
+
+        sldQty.valueProperty().addListener((obs, oldValue, newValue) -> {
+            int value = newValue.intValue();
+
+            // Habilitar y mostrar los campos de texto de jugadores adicionales según el
+            // valor del slider
+            txfJug3.setDisable(value < 3);
+            txfJug4.setDisable(value < 4);
+            txfJug5.setDisable(value < 5);
+            txfJug6.setDisable(value < 6);
+            txfJug3.setVisible(value >= 3);
+            txfJug4.setVisible(value >= 4);
+            txfJug5.setVisible(value >= 5);
+            txfJug6.setVisible(value >= 6);
+        });
+
     }
 
 }
