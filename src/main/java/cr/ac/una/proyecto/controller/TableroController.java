7c98aac529df53114fd2dd2363bf2ee14d901c9d
Alejandro Leon Marin <aleleonmarin01@gmail.com>
2024-05-04 11:02:14 -0600

    [UI] TableroView UPDATED

--- src/main/java/cr/ac/una/proyecto/controller/TableroController.java
+++ src/main/java/cr/ac/una/proyecto/controller/TableroController.java
@@ -6,15 +6,21 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.ResourceBundle;
 import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
 import javafx.animation.RotateTransition;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.transform.Rotate;
 import javafx.util.Duration;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.EventHandler;
 
 /**
  * FXML Controller class
@@ -23,7 +29,10 @@ import javafx.util.Duration;
  */
 public class TableroController extends Controller implements Initializable {
 
+    @FXML
     private Pane tablero;
+    @FXML
+    private Label lblTiempo;
     private static final int RADIO_TABLERO = 400; // Radio del tablero circular
     @FXML
     private ImageView ruleta;
@@ -37,6 +46,7 @@ public class TableroController extends Controller implements Initializable {
     private RotateTransition rotate;
     private ArrayList<String> opciones = new ArrayList<>(Arrays.asList("Deporte", "Arte", "Geografia", "Entretenimiento", "Ciencia", "Historia"));
     private double anguloInicial = 0;
+    private int segundos = 0;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -45,6 +55,20 @@ public class TableroController extends Controller implements Initializable {
 
     @Override
     public void initialize() {
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                segundos++;
+                int minutos = segundos / 60;
+                int seg = segundos % 60;
+                lblTiempo.setText(String.format("Tiempo: %02d:%02d", minutos, seg));
+            }
+        }));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+
+
 
     }
 
@@ -58,7 +82,7 @@ public class TableroController extends Controller implements Initializable {
         rotate.setNode(ruleta);
         ruleta.setRotate(anguloInicial);
 
-        rotate.setDuration(Duration.millis(3));
+        rotate.setDuration(Duration.seconds(3));
         rotate.setInterpolator(Interpolator.EASE_BOTH);
 
         double nuevoAnguloDetenido = generarAnguloAleatorio();
