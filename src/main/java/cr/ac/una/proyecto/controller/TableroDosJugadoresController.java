b509781a4e82f19647098709e85565e5075aaa6c
AleLeonMarin <aleleonmarin01@gmail.com>
2024-05-06 19:38:12 -0600

    [ADD] Tableros

---
+++ src/main/java/cr/ac/una/proyecto/controller/TableroDosJugadoresController.java
@@ -0,0 +1,37 @@
+package cr.ac.una.proyecto.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+
+public class TableroDosJugadoresController extends Controller implements Initializable {
+
+    @FXML
+    private MFXButton btnPicker;
+
+    @FXML
+    private ImageView ruleta;
+
+    @FXML
+    void onActionBtnPicker(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void initialize() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
