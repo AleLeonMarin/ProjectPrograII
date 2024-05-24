f3a4c0e7b6d8392745618ccf265d280a525d7021
JustinMdz <justin.mendez.mena@est.una.ac.cr>
2024-05-24 15:47:26 -0600

    All boards are managed with the same controller

--- src/main/java/cr/ac/una/proyecto/controller/TablerosController.java
+++ src/main/java/cr/ac/una/proyecto/controller/TablerosController.java
@@ -5,42 +5,48 @@ import cr.ac.una.proyecto.model.Sector;
 import cr.ac.una.proyecto.util.Animacion;
 import cr.ac.una.proyecto.util.AppContext;
 import cr.ac.una.proyecto.util.FlowController;
-import cr.ac.una.proyecto.util.Ruleta;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
-import javafx.application.Platform;
-
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.application.Platform;
 
-public class TableroDosJugadoresController extends Controller implements Initializable {
+public class TablerosController extends Controller implements Initializable {
 
     @FXML
     private ImageView imvRuleta;
-
-    private Animacion animacion;
-    private String rutaImagen = "/cr/ac/una/proyecto/resources/HistoriaC.png";
-
     @FXML
     private GridPane grdpTablero;
-
-    @FXML
-    private ImageView imvCarta;
     @FXML
     private ImageView imvPicker;
 
+    private Animacion animacion;
     private Juego juego;
     ArrayList<Sector> sectores;
+    @FXML
+    private ImageView imvCarta;
 
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public void initialize() {
+        iniciarClases();
+        cargarSectores();
 
+        juego.cargarDatosImagenes(grdpTablero);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+
+    @FXML
+    private void OnMouseClickedPicker(MouseEvent event) {
+        moverRuleta();
     }
 
     private void cargarSectores() {
@@ -53,34 +59,9 @@ public class TableroDosJugadoresController extends Controller implements Initial
         }
     }
 
-    private void cargarDatos() {
-        animacion = new Animacion();
-    }
-
-    private void cargarDatosImagenes() {
-        imvCarta.setImage(new Image(getClass().getResourceAsStream(rutaImagen)));
-    }
-
-    @Override
-    public void initialize() {
+    private void iniciarClases() {
         juego = new Juego();
-        cargarDatos();
-        cargarDatosImagenes();
-        cargarSectores();
-
-        juego.cargarDatosImagenes(grdpTablero);
-    }
-
-    @FXML
-    private void moverTarjeta(MouseEvent event) {
-        Animacion animacion = new Animacion();
-        Stage stage = (Stage) imvCarta.getScene().getWindow();
-        animacion.saltoTarjeta(imvCarta, stage);
-    }
-
-    @FXML
-    private void OnMouseClickedPicker(MouseEvent event) {
-        moverRuleta();
+        animacion = new Animacion();
     }
 
     private void moverRuleta() {
@@ -92,20 +73,19 @@ public class TableroDosJugadoresController extends Controller implements Initial
         {
             System.out.println("La animación de la ruleta ha terminado en esta categoria: " + categoria + ", Angulo: " + anguloDetenido);
             Platform.runLater(() -> llamarPreguntaView());
-            // Aquí puedes realizar cualquier acción adicional que desees
-
         };
 
-        // Llamar al método animacionRuleta para iniciar la animación de la ruleta
         animacion.animacionRuleta(imvRuleta, anguloDetenido, onFinish);
-
-        // Aquí puedes actualizar la UI o realizar cualquier acción necesaria con la categoría resultante
         AppContext.getInstance().set("preguntaCategoria", categoria);
         AppContext.getInstance().set("preguntaJugador", juego.getJugadorPregunta());
     }
 
     private void llamarPreguntaView() {
-        FlowController.getInstance().goViewInWindowModal("preguntaView", ((Stage) imvCarta.getScene().getWindow()), true);
+        FlowController.getInstance().goViewInWindowModal("preguntaView", ((Stage) imvRuleta.getScene().getWindow()), true);
+    }
+
+    @FXML
+    private void moverTarjeta(MouseEvent event) {
     }
 
 }
